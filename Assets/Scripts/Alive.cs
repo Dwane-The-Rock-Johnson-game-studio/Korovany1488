fcde700bda86dfb634a09c022d97f0e36c8a623a
Jayzar-web <roflanblch@gmail.com>
2023-12-27 12:17:23 +0600

    added the Alive class

---
+++ Assets/Scripts/Alive.cs
@@ -0,0 +1,13 @@
+
+
+public abstract class Alive : GameEntity
+{
+    public float Health { get; private set; }
+    public float Stamina { get; private set; }
+    public float Stealth { get; private set; }
+
+    public void GetDamage(float damage) 
+    { 
+
+    }
+}
