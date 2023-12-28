0fdc8ad2cca50421fe5f9fa35a123dec465ca825
Almat <vip.akvan@gmail.com>
2023-12-28 09:51:08 +0600

    added an Antropomorph class

---
+++ Assets/Scripts/Antropomorph.cs
@@ -0,0 +1,16 @@
+public abstract class Antropomorphic : Alive
+{
+    public Helmet Helmet { get; set; }      
+    public Body Body { get; set; }
+    public Pants Pants { get; set; }
+    public Shoes Shoes { get; set; }
+    public Weapon RightHand { get; set; }
+    public Weapon LeftHand { get; set; }
+    public Inventory Inventory { get; set; }
+
+    public void Eat(Food food)
+    {
+
+    }
+
+}
