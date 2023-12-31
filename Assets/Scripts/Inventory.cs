8543d5baa543d57b04f910e8f2236a87fa638177
Vya4slav0 <vya4slav4617@gmail.com>
2023-12-31 17:56:31 +0600

    Added method to store items in inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,9 +1,26 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public class Inventory : GameEntity
 {
     public int MaxCapacity { get; private set; }
+    public int CurrentCapacity => ListThings.Sum(t => t.Volume);
     public bool IsInfinity { get; private set; }
     public List<IStorable> ListThings { get; private set; }
     public bool CanEquip { get; private set; }
+
+    public Inventory(int maxCapacity, bool canEquip, bool isInfinity = false)
+    {
+        MaxCapacity = maxCapacity;
+        CanEquip = canEquip;
+        IsInfinity = isInfinity;
+        ListThings = new List<IStorable>();
+    }
+
+    public bool StoreItem(IStorable storableItem)
+    {
+        if (!IsInfinity && CurrentCapacity + storableItem.Volume > MaxCapacity) return false;
+        ListThings.Add(storableItem);
+        return true;
+    }
 }
